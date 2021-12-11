19f960ece70260c5e5cc50b3ffeb89c280537f60
Brenton Farmer <brent.farmer@wagglebee.net>
2021-12-11 16:09:52 -0500

    switch to scoped namespace

--- src/Hyperbee.Migrations/Couchbase/ServiceCollectionExtensions.cs
+++ src/Hyperbee.Migrations/Couchbase/ServiceCollectionExtensions.cs
@@ -7,62 +7,60 @@ using System.Runtime.Loader;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Hyperbee.Migrations.Couchbase
+namespace Hyperbee.Migrations.Couchbase;
+
+public static class ServiceCollectionExtensions
 {
-    public static class ServiceCollectionExtensions
+    public static IServiceCollection AddCouchbaseMigrations( this IServiceCollection services )
     {
-        public static IServiceCollection AddCouchbaseMigrations( this IServiceCollection services )
-        {
-            return AddCouchbaseMigrations( services, null, Assembly.GetCallingAssembly() );
-        }
-
-        public static IServiceCollection AddCouchbaseMigrations( this IServiceCollection services, Action<CouchbaseMigrationOptions> configuration )
-        {
-            return AddCouchbaseMigrations( services, configuration, Assembly.GetCallingAssembly() );
-        }
-
-        private static IServiceCollection AddCouchbaseMigrations( IServiceCollection services, Action<CouchbaseMigrationOptions> configuration, Assembly defaultAssembly )
-        {
-            CouchbaseMigrationOptions CouchbaseMigrationOptionsFactory( IServiceProvider provider )
-            {
-                var options = new CouchbaseMigrationOptions( new DefaultMigrationActivator( provider ) );
-
-                // invoke the configuration
-
-                configuration?.Invoke( options );
-
-                // concat any options.Assemblies with IConfiguration `FromAssemblies` and
-
-                var config = provider.GetRequiredService<IConfiguration>();
-
-                var nameAssemblies = config
-                    .GetSection( "Migrations:FromAssemblies" )
-                    .GetArray<string>()
-                    .Select( name => Assembly.Load( new AssemblyName( name ) ) );
-
-                var pathAssemblies = config
-                    .GetSection( "Migrations:FromPaths" )
-                    .GetArray<string>()
-                    .Select( name => AssemblyLoadContext.Default.LoadFromAssemblyPath( Path.GetFullPath( name ) ) );
-
-                options.Assemblies = options.Assemblies
-                    .Concat( nameAssemblies )
-                    .Concat( pathAssemblies )
-                    .Distinct()
-                    .DefaultIfEmpty( defaultAssembly )
-                    .ToList();
-
-                return options;
-            }
-
-            services.AddSingleton( CouchbaseMigrationOptionsFactory );
-            services.AddSingleton<MigrationOptions>( provider => provider.GetRequiredService<CouchbaseMigrationOptions>() );
-            services.AddSingleton<IMigrationRecordStore, CouchbaseRecordStore>();
-            services.AddSingleton<MigrationRunner>();
-
-            return services;
-        }
-
-        private static T[] GetArray<T>( this IConfiguration section ) => section.Get<T[]>() ?? Array.Empty<T>();
+        return AddCouchbaseMigrations( services, null, Assembly.GetCallingAssembly() );
     }
+
+    public static IServiceCollection AddCouchbaseMigrations( this IServiceCollection services, Action<CouchbaseMigrationOptions> configuration )
+    {
+        return AddCouchbaseMigrations( services, configuration, Assembly.GetCallingAssembly() );
+    }
+
+    private static IServiceCollection AddCouchbaseMigrations( IServiceCollection services, Action<CouchbaseMigrationOptions> configuration, Assembly defaultAssembly )
+    {
+        CouchbaseMigrationOptions CouchbaseMigrationOptionsFactory( IServiceProvider provider )
+        {
+            var options = new CouchbaseMigrationOptions( new DefaultMigrationActivator( provider ) );
+
+            // invoke the configuration
+
+            configuration?.Invoke( options );
+
+            // concat any options.Assemblies with IConfiguration `FromAssemblies` and
+
+            var config = provider.GetRequiredService<IConfiguration>();
+
+            var nameAssemblies = config
+                .GetEnumerable<string>( "Migrations:FromAssemblies" )
+                .Select( name => Assembly.Load( new AssemblyName( name ) ) );
+
+            var pathAssemblies = config
+                .GetEnumerable<string>( "Migrations:FromPaths" )
+                .Select( name => AssemblyLoadContext.Default.LoadFromAssemblyPath( Path.GetFullPath( name ) ) );
+
+            options.Assemblies = options.Assemblies
+                .Concat( nameAssemblies )
+                .Concat( pathAssemblies )
+                .Distinct()
+                .DefaultIfEmpty( defaultAssembly )
+                .ToList();
+
+            return options;
+        }
+
+        services.AddSingleton( CouchbaseMigrationOptionsFactory );
+        services.AddSingleton<MigrationOptions>( provider => provider.GetRequiredService<CouchbaseMigrationOptions>() );
+        services.AddSingleton<IMigrationRecordStore, CouchbaseRecordStore>();
+        services.AddSingleton<MigrationRunner>();
+
+        return services;
+    }
+
+    private static IEnumerable<T> GetEnumerable<T>( this IConfiguration config, string key ) 
+        => config.GetSection( key ).Get<IEnumerable<T>>() ?? Enumerable.Empty<T>();
 }
